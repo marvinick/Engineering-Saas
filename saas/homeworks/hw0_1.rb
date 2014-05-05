668b15f9fb3aa03f7c8008ef73c22c8d11ba5da8
marvinick <marviluw@gmail.com>
2014-05-05 08:08:30 -0500

    homework

---
+++ saas/homeworks/hw0_1.rb
@@ -0,0 +1,7 @@
+#HW0 PART 1  (100 points possible)
+#Check the documentation on Array, Hash and Enumerable as they could help tremendously with these exercises. :-)
+
+#Define a method sum which takes an array of integers as an argument and returns the sum of its elements. For an empty array it should return zero.
+#Define a method max_2_sum which takes an array of integers as an argument and returns the sum of its two largest elements. For an empty array it should return zero. For an array with just one element, it should return that element.
+#Define a method sum_to_n? which takes an array of integers and an additional integer, n, as arguments and returns true if any two distinct elements in the array of integers sum to n. An empty array or single element array should both return false.
+
