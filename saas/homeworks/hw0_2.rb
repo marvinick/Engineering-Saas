7e86c9f24abc9ae9566c78c8d2b3b3e365597392
marvinick <marviluw@gmail.com>
2014-05-09 08:52:37 -0500

    regex

--- saas/homeworks/hw0_2.rb
+++ saas/homeworks/hw0_2.rb
@@ -2,6 +2,9 @@
 
 #Define a method hello(name) that takes a string representing a name and returns the string "Hello, " concatenated with the name.
 #Define a method starts_with_consonant?(s) that takes a string and returns true if it starts with a consonant and false otherwise. (For our purposes, a consonant is any letter other than A, E, I, O, U.) NOTE: be sure it works for both upper and lower case and for nonletters!
+  def starts_with_consonant?(s)
+   !!(s =~  /^[bcdfghjklmnpqrstvwxyz]/i)
+  end
 #Define a method binary_multiple_of_4?(s) that takes a string and returns true if the string represents a binary number that is a multiple of 4. NOTE: be sure it returns false if the string is not a valid binary number!
 
 def hello(name)
